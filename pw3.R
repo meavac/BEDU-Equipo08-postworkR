310c37fde34fe1c2465335b8e251ee77651754de
CarlosJiZe <68188255+CarlosJiZe@users.noreply.github.com>
2021-06-25 21:10:18 -0500

    Update pw3.R

--- pw3.R
+++ pw3.R
@@ -1,3 +1,21 @@
+#Postwork 3
+
+# Instrucciones:
+
+# 1.- Con el último data frame obtenido en el postwork de la sesión 2, elabora tablas de frecuencias relativas para estimar las siguientes probabilidades:
+   #La probabilidad (marginal) de que el equipo que juega en casa anote x goles (x=0,1,2,)
+   #La probabilidad (marginal) de que el equipo que juega como visitante anote y goles (y=0,1,2,)
+   #La probabilidad (conjunta) de que el equipo que juega en casa anote x goles y el equipo que juega como visitante anote y goles (x=0,1,2,, y=0,1,2,)
+
+# 2.- Realiza lo siguiente:
+   #Un gráfico de barras para las probabilidades marginales estimadas del número de goles que anota el equipo de casa.
+   #Un gráfico de barras para las probabilidades marginales estimadas del número de goles que anota el equipo visitante.
+   #Un HeatMap para las probabilidades conjuntas estimadas de los números de goles que anotan el equipo de casa y el equipo visitante en un partido.
+
+#Notas para los datos de soccer: https://www.football-data.co.uk/notes.txt
+
+#SOLUCIÓN:
+
 # Cargamos dplyr y leemos los datos csv
 packs <- c("dplyr", "ggplot2", "plotly")
 lapply(packs, library, character.only = TRUE)
