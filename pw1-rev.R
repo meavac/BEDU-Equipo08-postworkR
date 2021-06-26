d68b9f6327654186d0b9273326e06baa583b4964
Alejandro Gómora <86501233+agomora08@users.noreply.github.com>
2021-06-26 10:17:01 -0500

    Update pw1-rev.R

--- pw1-rev.R
+++ pw1-rev.R
@@ -21,32 +21,31 @@
 # Importamos dplyr para usar select()
 library(dplyr)
 
-# Leemos los datos y los agregamos a una variable
+# Obtenemos el working directory, leemos los datos y los agregamos a la variable
 data <- read.csv("https://www.football-data.co.uk/mmz4281/1920/SP1.csv")
 
 # Seleccionamos las columnas necesarias y creamos una tabla para cada ejercicio
 data.FTHG.table <- table(select(data, FTHG), dnn = "Prob. marginal equipo casa anote x goles")
-
 data.FTAG.table <- table(select(data, FTAG), dnn = "Prob. marginal equipo afuera anote x goles")
-
 data.goals.table <- table(select(data, FTHG, FTAG), dnn = c("Goles de casa", "Goles de afuera"))
 
 # Dividimos los resultados de las tablas entre la cantiad de renglones, para obtener la probabilidad marginal y compuesta de cada valor
 n <- nrow(data) # Obtenemos la cantidad de renglones (en este caso 1 renglon = 1 juego)
 
 (data.FTHG.table / n) # Prob marginal x goles de casa en decimal
-
 (data.FTAG.table / n) # Prob marginal x goles de afuera en decimal
-
-data.goals.table <- data.goals.table / n # Prob compuesta que los dos equipos anoten x goles en decimal
+data.goals.table <- (data.goals.table / n) # Prob compuesta que los dos equipos anoten x goles en decimal
 
 # Creamos vectores aleatorios que representan la cantidad de goles para el equipo de casa y los visitantes
-(data.goals.table <- addmargins(data.goals.table))
-x <- sample(c(0:5),1,F)
-y <- sample(c(0:6),1,F)
-paste("La probabilidad de que el equipo que juega como visitante anote", x, "goles es de",
-      round(data.goals.table[8, x+1]*100,4),"%")
-paste("La probabilidad de que el equipo que juega en casa anote", y, "goles es de",
-      round(data.goals.table[y+1,7]*100,4),"%")
-paste("La probabilidad de que el equipo que juega en casa anote", x, "goles y el equipo que juega como visitante anote" , y, "goles es de",
+data.goals.table <- addmargins(data.goals.table)
+data.goals.table
+vec1 <- c(0:6)
+vec2 <- c(0:5)
+x <- sample(vec1,1,F)
+y <- sample(vec2,1,F)
+paste("La probabilidad de que el equipo que juega en casa anote", x, "gol(es) es de",
+      round(data.goals.table[x+1,7]*100,4),"%")
+paste("La probabilidad de que el equipo que juega como visitante anote", y, "gol(es) es de",
+      round(data.goals.table[8,y+1]*100,4),"%")
+paste("La probabilidad de que el equipo que juega en casa anote", x, "gol(es) y el equipo que juega como visitante anote" , y, "gol(es) es de",
       round(data.goals.table[x+1,y+1]*100,4),"%")
