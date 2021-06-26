da5f78b646be7cb5b9f45610c5ea0d0b044c06ca
CarlosJiZe <68188255+CarlosJiZe@users.noreply.github.com>
2021-06-25 21:16:46 -0500

    Update pw4.R

--- pw4.R
+++ pw4.R
@@ -6,7 +6,7 @@
 # anote Y=y goles (y=0,1,... ,6), en un partido. Obtén una tabla de cocientes al dividir estas probabilidades conjuntas por el producto de las probabilidades 
 # marginales correspondientes.
 
-# 2.-Mediante un procedimiento de boostrap, obtén más cocientes similares a los obtenidos en la tabla del punto anterior.
+# 2.- Mediante un procedimiento de boostrap, obtén más cocientes similares a los obtenidos en la tabla del punto anterior.
 # Esto para tener una idea de las distribuciones de la cual vienen los cocientes en la tabla anterior. Menciona en cuáles casos le parece razonable
 # suponer que los cocientes de la tabla en el punto 1, son iguales a 1 (en tal caso tendríamos independencia de las variables aleatorias X y Y).
 
