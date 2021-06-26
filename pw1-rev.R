c06d5dc9db41b9a1c86ab1da944b618c5acc8a4a
Alejandro Gómora <86501233+agomora08@users.noreply.github.com>
2021-06-26 10:32:08 -0500

    Update pw1-rev.R

--- pw1-rev.R
+++ pw1-rev.R
@@ -26,15 +26,15 @@ data <- read.csv("https://www.football-data.co.uk/mmz4281/1920/SP1.csv")
 
 # Seleccionamos las columnas necesarias y creamos una tabla para cada ejercicio
 data.FTHG.table <- table(select(data, FTHG), dnn = "Prob. marginal equipo casa anote x goles")
-data.FTAG.table <- table(select(data, FTAG), dnn = "Prob. marginal equipo afuera anote x goles")
+data.FTAG.table <- table(select(data, FTAG), dnn = "Prob. marginal equipo afuera anote y goles")
 data.goals.table <- table(select(data, FTHG, FTAG), dnn = c("Goles de casa", "Goles de afuera"))
 
 # Dividimos los resultados de las tablas entre la cantiad de renglones, para obtener la probabilidad marginal y compuesta de cada valor
-n <- nrow(data) # Obtenemos la cantidad de renglones (en este caso 1 renglon = 1 juego)
+n <- nrow(data) # Obtenemos la cantidad de renglones (en este caso 1 renglón = 1 juego)
 
-(data.FTHG.table / n) # Prob marginal x goles de casa en decimal
-(data.FTAG.table / n) # Prob marginal x goles de afuera en decimal
-data.goals.table <- (data.goals.table / n) # Prob compuesta que los dos equipos anoten x goles en decimal
+(data.FTHG.table / n) # Prob marginal: x goles de casa en decimal
+(data.FTAG.table / n) # Prob marginal: y goles de afuera en decimal
+data.goals.table <- (data.goals.table / n) # Prob compuesta que los dos equipos anoten z goles en decimal
 
 # Creamos vectores aleatorios que representan la cantidad de goles para el equipo de casa y los visitantes
 data.goals.table <- addmargins(data.goals.table)
