93df6d5540a0081407753c331c2089b2917594e9
agomora08 <86501233+agomora08@users.noreply.github.com>
2021-06-26 01:46:26 -0500

    Update pw4.R

--- pw4.R
+++ pw4.R
@@ -2,11 +2,11 @@
 
 # Instrucciones:
 
-# 1.- Ya hemos estimado las probabilidades conjuntas de que el equipo de casa anote X=x goles (x=0,1,... ,8), y el equipo visitante
+# 1. Ya hemos estimado las probabilidades conjuntas de que el equipo de casa anote X=x goles (x=0,1,... ,8), y el equipo visitante
 # anote Y=y goles (y=0,1,... ,6), en un partido. Obtén una tabla de cocientes al dividir estas probabilidades conjuntas por el producto de las probabilidades 
 # marginales correspondientes.
 
-# 2.- Mediante un procedimiento de boostrap, obtén más cocientes similares a los obtenidos en la tabla del punto anterior.
+# 2. Mediante un procedimiento de boostrap, obtén más cocientes similares a los obtenidos en la tabla del punto anterior.
 # Esto para tener una idea de las distribuciones de la cual vienen los cocientes en la tabla anterior. Menciona en cuáles casos le parece razonable
 # suponer que los cocientes de la tabla en el punto 1, son iguales a 1 (en tal caso tendríamos independencia de las variables aleatorias X y Y).
 
@@ -40,8 +40,6 @@ n <- nrow(games.goals)
 FTHG.mprob <- round(table(games.FTHG) / n, 4)
 FTAG.mprob <- round(table(games.FTAG) / n, 4)
 games.cprob <- round(table(games.goals) / n, 4)
-
-# Postwork 4
 
 # Definimos unas variables que usaremos en el loop
 quot.table <- games.cprob
