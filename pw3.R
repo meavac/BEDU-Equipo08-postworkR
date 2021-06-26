8d44b54bc5bb5542b50bb81b0fd00545ec9d2019
Melina Ávila <85912223+meavac@users.noreply.github.com>
2021-06-25 22:53:52 -0500

    Update pw3.R

--- pw3.R
+++ pw3.R
@@ -69,19 +69,33 @@ games.cprob.df <- as.data.frame(games.cprob)
 # Creamos las gráficas
 data.games.heatmap <- ggplot(games.cprob.df, aes(x=FTHG, y=FTAG, fill=Freq)) + 
   geom_tile() +
-  xlab("Equipo de casa anota x goles") +
-  ylab("Equipo de afuera anota y goles") +
-  scale_fill_continuous(name = "Prob. Conjunta")
+  xlab("Goles en casa") +
+  ylab("Goles de visitante") +
+  labs(fill= "Probabilidad", 
+  title = paste("Heatmap de la probabilidad conjunta de las anotaciones"))+
+  theme(plot.title = element_text(size=12))+
+  scale_fill_continuous(name = "Prob. Conjunta")+
+  scale_fill_gradient(low = "#f13c77", high = "#f5e6ad") +
+  theme(plot.margin=margin(20,20,20,15),
+        title =element_text(face='bold', color = "#71092A"))
 
 data.FTHG.bar <- ggplot(FTHG.mprob.df, aes(x=games.FTHG, y=Freq)) + 
-  geom_bar(stat="identity") +
-  xlab("Equipo de casa anota x goles") +
-  ylab("Probabilidad marginal")
+  geom_bar(stat="identity", color="#F02D3A", fill="#273043") +
+  theme_dark()+
+  xlab("Goles anotados") +
+  ylab("Porcentaje")+
+  labs(title = paste("Probabilidad marginal. Anotaciones del equipo en casa"))+
+  theme(plot.margin=margin(25,20,30,25),
+        title =element_text(size=9, face='bold', color = "#273043"))
 
 data.FTAG.bar <- ggplot(FTAG.mprob.df, aes(x=games.FTAG, y=Freq)) + 
-  geom_bar(stat="identity") +
-  xlab("Equipo de afuera anota x goles") +
-  ylab("Probabilidad marginal")
+  geom_bar(stat="identity", color="#99E1D9", fill="#32292F") +
+  xlab("Goles anotados") +
+  ylab("Porcentaje")+
+  labs(title = paste("Probabilidad marginal. Anotaciones del equipo visitante"))+
+  theme(plot.margin=margin(25,25,20,20),
+        title =element_text(size=9, face='bold', color = "#32292F"))
+
 
 # Usamos plotly junto con ggplot2 para meter tooltips a las gráficas
 
