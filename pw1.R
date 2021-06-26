06a859f751ab69f9514588b1359ee6b85bfa4781
CarlosJiZe <68188255+CarlosJiZe@users.noreply.github.com>
2021-06-25 20:59:38 -0500

    Update pw1.R

--- pw1.R
+++ pw1.R
@@ -1,3 +1,21 @@
+# Postwork 1
+
+#Instrucciónes:
+ 
+#1. Importa los datos de soccer de la temporada 2019/2020 de la primera división de la liga española a `R`, los datos los puedes encontrar en el siguiente enlace: https://www.football-data.co.uk/spainm.php
+#2. Del data frame que resulta de importar los datos a `R`, extrae las columnas que contienen los números de goles anotados por los equipos que jugaron en casa (FTHG) y los goles anotados por los equipos que jugaron como visitante (FTAG)
+#3. Consulta cómo funciona la función table en R al ejecutar en la consola ?table
+
+#Posteriormente elabora tablas de frecuencias relativas para estimar las siguientes probabilidades:
+
+#La probabilidad (marginal) de que el equipo que juega en casa anote x goles (x = 0, 1, 2, ...)
+#La probabilidad (marginal) de que el equipo que juega como visitante anote y goles (y = 0, 1, 2, ...)
+#La probabilidad (conjunta) de que el equipo que juega en casa anote x goles y el equipo que juega como visitante anote y goles (x = 0, 1, 2, ..., y = 0, 1, 2, ...)
+
+#Notas para los datos de soccer: https://www.football-data.co.uk/notes.txt
+
+# SOLUCIÓN:
+
 # Importamos dplyr para usar select()
 library(dplyr)
 
