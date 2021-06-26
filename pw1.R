cd394936f1093861568df474dade67324a71198c
agomora08 <86501233+agomora08@users.noreply.github.com>
2021-06-25 21:03:49 -0500

    Update pw1.R

--- pw1.R
+++ pw1.R
@@ -1,18 +1,18 @@
 # Postwork 1
 
-#Instrucciónes:
+# Instrucciones:
  
-#1. Importa los datos de soccer de la temporada 2019/2020 de la primera división de la liga española a R, los datos los puedes encontrar en el siguiente enlace: https://www.football-data.co.uk/spainm.php
-#2. Del data frame que resulta de importar los datos a R, extrae las columnas que contienen los números de goles anotados por los equipos que jugaron en casa (FTHG) y los goles anotados por los equipos que jugaron como visitante (FTAG)
-#3. Consulta cómo funciona la función table en R al ejecutar en la consola ?table
+# 1. Importa los datos de soccer de la temporada 2019/2020 de la primera división de la liga española a R, los datos los puedes encontrar en el siguiente enlace: https://www.football-data.co.uk/spainm.php
+# 2. Del data frame que resulta de importar los datos a R, extrae las columnas que contienen los números de goles anotados por los equipos que jugaron en casa (FTHG) y los goles anotados por los equipos que jugaron como visitante (FTAG)
+# 3. Consulta cómo funciona la función table en R al ejecutar en la consola ?table
 
-#Posteriormente elabora tablas de frecuencias relativas para estimar las siguientes probabilidades:
+# Posteriormente elabora tablas de frecuencias relativas para estimar las siguientes probabilidades:
 
-#La probabilidad (marginal) de que el equipo que juega en casa anote x goles (x = 0, 1, 2, ...)
-#La probabilidad (marginal) de que el equipo que juega como visitante anote y goles (y = 0, 1, 2, ...)
-#La probabilidad (conjunta) de que el equipo que juega en casa anote x goles y el equipo que juega como visitante anote y goles (x = 0, 1, 2, ..., y = 0, 1, 2, ...)
+# La probabilidad (marginal) de que el equipo que juega en casa anote x goles (x = 0, 1, 2, ...)
+# La probabilidad (marginal) de que el equipo que juega como visitante anote y goles (y = 0, 1, 2, ...)
+# La probabilidad (conjunta) de que el equipo que juega en casa anote x goles y el equipo que juega como visitante anote y goles (x = 0, 1, 2, ..., y = 0, 1, 2, ...)
 
-#Notas para los datos de soccer: https://www.football-data.co.uk/notes.txt
+# Notas para los datos de soccer: https://www.football-data.co.uk/notes.txt
 
 # SOLUCIÓN:
 
