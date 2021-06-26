a0d1cd5c7aa66ca312532cdc41cf119450ec0589
armando-lv <83714394+armando-lv@users.noreply.github.com>
2021-06-26 00:49:55 -0700

    Add files via upload

---
+++ pw1-rev.R
@@ -0,0 +1,52 @@
+# Postwork 1
+
+# Instrucciones:
+
+# 1. Importa los datos de soccer de la temporada 2019/2020 de la primera división de la liga española a R, 
+# los datos los puedes encontrar en el siguiente enlace: https://www.football-data.co.uk/spainm.php.
+# 2. Del data frame que resulta de importar los datos a R, extrae las columnas que contienen los números 
+# de goles anotados por los equipos que jugaron en casa (FTHG) y los goles anotados por los equipos que jugaron como visitante (FTAG).
+# 3. Consulta cómo funciona la función table en R al ejecutar en la consola ?table.
+
+# Posteriormente elabora tablas de frecuencias relativas para estimar las siguientes probabilidades:
+
+# La probabilidad (marginal) de que el equipo que juega en casa anote x goles (x = 0, 1, 2, ...)
+# La probabilidad (marginal) de que el equipo que juega como visitante anote y goles (y = 0, 1, 2, ...)
+# La probabilidad (conjunta) de que el equipo que juega en casa anote x goles y el equipo que juega como visitante anote y goles (x = 0, 1, 2, ..., y = 0, 1, 2, ...)
+
+# Notas para los datos de soccer: https://www.football-data.co.uk/notes.txt
+
+# SOLUCIÓN:
+
+# Importamos dplyr para usar select()
+library(dplyr)
+
+# Leemos los datos y los agregamos a una variable
+data <- read.csv("https://www.football-data.co.uk/mmz4281/1920/SP1.csv")
+
+# Seleccionamos las columnas necesarias y creamos una tabla para cada ejercicio
+data.FTHG.table <- table(select(data, FTHG), dnn = "Prob. marginal equipo casa anote x goles")
+
+data.FTAG.table <- table(select(data, FTAG), dnn = "Prob. marginal equipo afuera anote x goles")
+
+data.goals.table <- table(select(data, FTHG, FTAG), dnn = c("Goles de casa", "Goles de afuera"))
+
+# Dividimos los resultados de las tablas entre la cantiad de renglones, para obtener la probabilidad marginal y compuesta de cada valor
+n <- nrow(data) # Obtenemos la cantidad de renglones (en este caso 1 renglon = 1 juego)
+
+(data.FTHG.table / n) # Prob marginal x goles de casa en decimal
+
+(data.FTAG.table / n) # Prob marginal x goles de afuera en decimal
+
+data.goals.table <- data.goals.table / n # Prob compuesta que los dos equipos anoten x goles en decimal
+
+# Creamos vectores aleatorios que representan la cantidad de goles para el equipo de casa y los visitantes
+(data.goals.table <- addmargins(data.goals.table))
+x <- sample(c(0:5),1,F)
+y <- sample(c(0:6),1,F)
+paste("La probabilidad de que el equipo que juega como visitante anote", x, "goles es de",
+      round(data.goals.table[8, x+1]*100,4),"%")
+paste("La probabilidad de que el equipo que juega en casa anote", y, "goles es de",
+      round(data.goals.table[y+1,7]*100,4),"%")
+paste("La probabilidad de que el equipo que juega en casa anote", x, "goles y el equipo que juega como visitante anote" , y, "goles es de",
+      round(data.goals.table[x+1,y+1]*100,4),"%")
